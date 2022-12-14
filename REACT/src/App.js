31097d3ef13ed7242af056f6f6fede85c829be7a
Steven DeLitta <sdelitta@gmail.com>
2022-12-14 13:36:09 -0500

    Testing image switch depending on cursor relation to follower, left or right

--- REACT/src/App.js
+++ REACT/src/App.js
@@ -1,32 +1,85 @@
-import React from "react"
-import { useState } from "react"
+import React, { useEffect, useState } from "react";
 import "./CSS/Boo.css"
 
 export const App = () => {
-  const cursorRef = React.useRef(null)
-  const [delayHandler, setDelayHandler] = useState(null)
-  const [data, setData] = useState(null)
-  // const [booState, setBooState] = useState(null)
-  console.log(data)
+  const cursorRef = React.useRef(null);
+  const [data, setData] = useState(null);
+  const [isMouseMoving, setIsMouseMoving] = useState(null);
+  const [timerVal, setTimerVal] = useState(0);
+  const [booLeft, setBooLeft] = useState(null)
+  const [booRight, setBooRight] = useState(null)
   
-  React.useEffect(() => {
-    document.addEventListener("mousemove", (event) => {
-      setDelayHandler(setTimeout(() => {
+  
+  useEffect(() => {
+    document.addEventListener("mousemove", event => {
       const { clientX, clientY } = event;
-      const mouseX = clientX - cursorRef.current.clientWidth / 100
-      const mouseY = clientY - cursorRef.current.clientHeight / 1
-      cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`
-      setData(mouseX, mouseY)
-        }, 1000))
+      const mouseX = clientX - cursorRef.current.clientWidth / 70;
+      const mouseY = clientY - cursorRef.current.clientHeight / .7;
+      cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+      setIsMouseMoving(true);
+      setTimerVal(0);
+      
+      // var follower = document.getElementById("follower")
+      // if (mouseX > follower.offsetLeft + follower.offsetWidth / 2) {
+      //   //cursor is to the right of the follower
+      //   follower.style.transform = "scalex(1)"
+      //   setBooRight(true)
+      //   setBooLeft(false)
+      // } else {
+      //   //cursor is to the left of the follower
+      //   follower.style.transform = 'scalex(-1)'
+      //   setBooRight(false)
+      //   setBooLeft(true)
+      // }
+
+    });
+  }, []);
+
+  // check if previous mouse position is equal to current mouse position
+  useEffect(() => {
+    console.log(timerVal)
+    if (timerVal > 100) {
+      setIsMouseMoving(false);
+    }
+  }, [timerVal])
+
+  useEffect(() => {
+    setInterval(() => {
+      setTimerVal(val => {
+        return val + 1;
+      })
+    }, 1)
+  }, [])
+    console.log(data)
+  
+  useEffect(() => {
+
+    var follower = document.getElementById("follower")
+    document.addEventListener('mousemove', function(event) {
+
+      var x = event.clientx;
+      var y = event.clienty;
+      if (x > follower.offsetLeft + follower.offsetWidth / 2) {
+        //cursor is to the right of the follower
+        follower.style.transform = "scalex(1)"
+        setBooRight(true)
+        setBooLeft(false)
+      } else {
+        //cursor is to the left of the follower
+        follower.style.transform = 'scalex(-1)'
+        setBooRight(false)
+        setBooLeft(true)
+      }
     })
   }, [])
-  clearTimeout(delayHandler)
   
   return (
     <div className="boo-parent">
-      <div className="blank"></div>
+      <div className="blank">
+        <p></p>
+      </div>
       <div className="boo">
-        {data ? <div className="cursor" ref={cursorRef} /> : <div className="cursorStop" ref={cursorRef}/>}
+        <div className={isMouseMoving && setBooRight ? "cursor" : !isMouseMoving && setBooRight ? "cursorStop" : isMouseMoving && setBooLeft ? "cursor-right" : "cursorStop-right"} ref={cursorRef} />
       </div>
     </div>
   )
