754bcef405d2bcb3ef641bc4d9ff881664a91eda
Steven DeLitta <sdelitta@gmail.com>
2022-12-14 14:15:57 -0500

    Testing image switch depending on relation between cursor and follower, left // right

--- src/App.js
+++ src/App.js
@@ -6,47 +6,67 @@ export const App = () => {
   const [data, setData] = useState(null);
   const [isMouseMoving, setIsMouseMoving] = useState(null);
   const [timerVal, setTimerVal] = useState(0);
+  const [booLeft, setBooLeft] = useState(null)
+  const [booRight, setBooRight] = useState(null)
   
-useEffect(() => {
-    document.addEventListener("mousemove", event => {
-    const { clientX, clientY } = event;
-      const mouseX = clientX - cursorRef.current.clientWidth / 100;
-      const mouseY = clientY - cursorRef.current.clientHeight;
-      cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-      setIsMouseMoving(true);
-    setTimerVal(0);
-    });
-}, []);
+  useEffect(() => {
+      document.addEventListener("mousemove", event => {
+      const { clientX, clientY } = event;
+        const mouseX = clientX - cursorRef.current.clientWidth / 100;
+        const mouseY = clientY - cursorRef.current.clientHeight;
+        cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+        setIsMouseMoving(true);
+      setTimerVal(0);
+      });
+  }, []);
 
-// check if previous mouse position is equal to current mouse position
-useEffect(() => {
-  console.log(timerVal)
-  if (timerVal > 100) {
-    setIsMouseMoving(false);
-  }
-}, [timerVal])
+  // check if previous mouse position is equal to current mouse position
+  useEffect(() => {
+    console.log(timerVal)
+    if (timerVal > 100) {
+      setIsMouseMoving(false);
+    }
+  }, [timerVal])
 
-useEffect(() => {
-  setInterval(() => {
-    setTimerVal(val => {
-      return val + 1;
+  useEffect(() => {
+    setInterval(() => {
+      setTimerVal(val => {
+        return val + 1;
+      })
+    }, 1)
+  }, [])
+    console.log(data)
+  
+  useEffect(() => {
+    var follower = document.getElementById("follower")
+    document.addEventListener('mousemove', function(event) {
+      var x = event.clientx;
+      var y = event.clienty;
+      if (x > follower.offsetLeft + follower.offsetWidth / 2) {
+        //cursor is to the right of the follower
+        follower.style.transform = "scalex(1)"
+        setBooRight(true)
+        setBooLeft(false)
+      } else {
+        //cursor is to the left of the follower
+        follower.style.transform = 'scalex(-1)'
+        setBooRight(false)
+        setBooLeft(true)
+      }
     })
-  }, 1)
-}, [])
-  console.log(data)
-  
+  }, [])
   
   return (
   <div className="App">
     <body>
-      <div className="boo-parent">
-        <div className="blank">
-          <p></p>
-        </div>
-        <div className="boo">
-          <div className={isMouseMoving ? "cursor" : "cursorStop"} ref={cursorRef} />
-        </div>
+    <div className="boo-parent">
+      <div className="blank">
+        <p></p>
       </div>
+      <div className="boo">
+        <div className={isMouseMoving && setBooRight ? "cursor" : !isMouseMoving && setBooRight ? "cursorStop" : isMouseMoving && setBooLeft ? "cursor-right" : "cursorStop-right"} ref={cursorRef} />
+      </div>
+    </div>
     </body>  
     <div className="footer-parent">
       <div className="footer">.</div>
