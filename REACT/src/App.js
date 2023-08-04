857fe372083786acb9b994bf0dbe6547d0196c32
Steven DeLitta <sdelitta@gmail.com>
2023-08-04 13:15:24 -0400

    Fixed orientation recognition

--- REACT/src/App.js
+++ REACT/src/App.js
@@ -3,86 +3,57 @@ import "./CSS/Boo.css"
 
 export const App = () => {
   const cursorRef = React.useRef(null);
-  const [data, setData] = useState(null);
-  const [isMouseMoving, setIsMouseMoving] = useState(null);
-  const [timerVal, setTimerVal] = useState(0);
-  const [booLeft, setBooLeft] = useState(null)
-  const [booRight, setBooRight] = useState(null)
-  
+  const [isMouseMoving, setIsMouseMoving] = useState(true);
+  const [booRight, setBooRight] = useState(true);
   
   useEffect(() => {
-    document.addEventListener("mousemove", event => {
+    const mouseMoveHandler = (event) => {
       const { clientX, clientY } = event;
-      const mouseX = clientX - cursorRef.current.clientWidth / 70;
+      const booPosition = cursorRef.current.getBoundingClientRect();
+      
+      // Determine if the boo should look right or left
+      const shouldLookRight = clientX > booPosition.left + booPosition.width / 2;
+      setBooRight(shouldLookRight);
+  
+      // Adjust the mouseX value based on the booRight state
+      const mouseXAdjustment = shouldLookRight ? -110 : 50; // Modify these values as needed
+      const mouseX = clientX + mouseXAdjustment;
       const mouseY = clientY - cursorRef.current.clientHeight / .7;
+  
       cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
       setIsMouseMoving(true);
-      setTimerVal(0);
-      
-      // var follower = document.getElementById("follower")
-      // if (mouseX > follower.offsetLeft + follower.offsetWidth / 2) {
-      //   //cursor is to the right of the follower
-      //   follower.style.transform = "scalex(1)"
-      //   setBooRight(true)
-      //   setBooLeft(false)
-      // } else {
-      //   //cursor is to the left of the follower
-      //   follower.style.transform = 'scalex(-1)'
-      //   setBooRight(false)
-      //   setBooLeft(true)
-      // }
+    };
+  
+    document.addEventListener("mousemove", mouseMoveHandler);
+    return () => document.removeEventListener("mousemove", mouseMoveHandler); // clear event listener on unmount
+  }, []);
+  
+  
 
-    });
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsMouseMoving(false); // Set isMouseMoving to false every 1000ms
+    }, 1000);
+    
+    return () => clearInterval(interval); // clear interval on unmount
   }, []);
 
-  // check if previous mouse position is equal to current mouse position
-  useEffect(() => {
-    console.log(timerVal)
-    if (timerVal > 100) {
-      setIsMouseMoving(false);
-    }
-  }, [timerVal])
-
-  useEffect(() => {
-    setInterval(() => {
-      setTimerVal(val => {
-        return val + 1;
-      })
-    }, 1)
-  }, [])
-    console.log(data)
-  
-  useEffect(() => {
-
-    var follower = document.getElementById("follower")
-    document.addEventListener('mousemove', function(event) {
-
-      var x = event.clientx;
-      var y = event.clienty;
-      if (x > follower.offsetLeft + follower.offsetWidth / 2) {
-        //cursor is to the right of the follower
-        follower.style.transform = "scalex(1)"
-        setBooRight(true)
-        setBooLeft(false)
-      } else {
-        //cursor is to the left of the follower
-        follower.style.transform = 'scalex(-1)'
-        setBooRight(false)
-        setBooLeft(true)
-      }
-    })
-  }, [])
-  
   return (
     <div className="boo-parent">
       <div className="blank">
         <p></p>
       </div>
       <div className="boo">
-        <div className={isMouseMoving && setBooRight ? "cursor" : !isMouseMoving && setBooRight ? "cursorStop" : isMouseMoving && setBooLeft ? "cursor-right" : "cursorStop-right"} ref={cursorRef} />
+        <div className={
+          isMouseMoving && booRight ? "cursor" : 
+          !isMouseMoving && booRight ? "cursorStop" : 
+          isMouseMoving && !booRight ? "cursor-right" : 
+          "cursorStop-right"} 
+          ref={cursorRef} 
+        />
       </div>
     </div>
-  )
+  );
 } 
 
-export default App
+export default App;
--- REACT/src/components/Boo.jsx
+++ REACT/src/components/Boo.jsx
@@ -1,34 +1,42 @@
-import React from "react"
-import { useState } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import "../CSS/Boo.css"
 
 export const Boo = () => {
-  const cursorRef = React.useRef(null)
-  const [delayHandler, setDelayHandler] = useState(null)
-  const [data, setData] = useState(null)
-//   const [booState, setBooState] = useState(null)
-console.log(data)
-  React.useEffect(() => {
-    document.addEventListener("mousemove", (event) => {
-      setDelayHandler(setTimeout(() => {
-      const { clientX, clientY } = event;
-      const mouseX = clientX - cursorRef.current.clientWidth / 100
-      const mouseY = clientY - cursorRef.current.clientHeight / 1
-      cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`
-      setData(mouseX, mouseY)
-        }, 1000))
-    })
+  const cursorRef = useRef(null)
+  const [isMovingRight, setIsMovingRight] = useState(true)
+
+  useEffect(() => {
+    const booMove = (event) => {
+      const { clientX, clientY } = event
+      const mouseX = clientX - cursorRef.current.clientWidth / 2
+      const mouseY = clientY - cursorRef.current.clientHeight / 2
+
+      // Determine if boo should look right or left
+      setIsMovingRight(mouseX > cursorRef.current.offsetLeft)
+
+      cursorRef.current.style.left = `${mouseX}px`
+      cursorRef.current.style.top = `${mouseY}px`
+    }
+
+    document.addEventListener("mousemove", booMove)
+
+    return () => {
+      document.removeEventListener("mousemove", booMove)
+    }
   }, [])
-  clearTimeout(delayHandler)
-  
+
+  const getCursorClass = () => {
+    return isMovingRight ? "cursor-right" : "cursor"
+  }
+
   return (
     <div className="boo-parent">
       <div className="blank"></div>
       <div className="boo">
-        {data ? <div className="cursor" ref={cursorRef} /> : <div className="cursorStop" ref={cursorRef}/>}
+        <div key={isMovingRight.toString()} className={getCursorClass()} ref={cursorRef} />
       </div>
     </div>
-  )
-} 
+  )  
+}
 
 export default Boo
